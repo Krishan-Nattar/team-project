492d70a0e6f048a068b1d62951bbcc3c166faaf2
Sally Sal <sallysal@alexs-imac.attlocal.net>
2019-09-18 10:57:36 -0700

    changed h1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Yo Its your pal young Sally Sal yo!</h1>
       <h2>
         Deployed with{' '}
         <a
